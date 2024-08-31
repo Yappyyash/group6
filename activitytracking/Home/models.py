3955f7f752b82020ba008c7b66a67cb22d2c43ff
Rajat Gupta <138861610+myCode2206@users.noreply.github.com>
2024-08-31 21:06:42 +0530

    Update models.py

--- activitytracking/Home/models.py
+++ activitytracking/Home/models.py
@@ -1,8 +1,11 @@
-# from django.db import models
 from pymongo import MongoClient
 
-client = MongoClient('mongodb://localhost:27017/')
+client = MongoClient(
+    'mongodb+srv://guptarajat2206:qHBUZpZ9f7UCSXH2@cluster0.u3ocn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    tls=True,
+    tlsAllowInvalidCertificates=True
+)
+# client = MongoClient('mongodb://localhost:27017/')
 db = client["MynewDB"]
+person_collection = db['user']
 
-
-person_collection=db['user']
