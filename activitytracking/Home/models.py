c76fb3a468a8c11426ab0499a52ef458638746e0
Rajat Gupta <138861610+myCode2206@users.noreply.github.com>
2024-08-31 21:38:42 +0530

    Update models.py

--- activitytracking/Home/models.py
+++ activitytracking/Home/models.py
@@ -1,11 +1,15 @@
 from pymongo import MongoClient
 
+#For Production
 client = MongoClient(
     'mongodb+srv://guptarajat2206:qHBUZpZ9f7UCSXH2@cluster0.u3ocn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
     tls=True,
     tlsAllowInvalidCertificates=True
 )
+
+#For Testing Purpose
 # client = MongoClient('mongodb://localhost:27017/')
+
 db = client["MynewDB"]
 person_collection = db['user']
 
